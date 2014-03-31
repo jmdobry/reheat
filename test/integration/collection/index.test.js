96f1df4911864fb854a9f186547a13656b3d6a19
Jason Dobry <jason.dobry@gmail.com>
2014-03-31 15:50:11 -0600

    Uncommented tests.

--- test/integration/collection/index.test.js
+++ test/integration/collection/index.test.js
@@ -4,21 +4,21 @@ module.exports = function (container) {
 		container.register('integration_collection_prototype_tests', require('./prototype/index.test'));
 
 		describe('/prototype', container.get('integration_collection_prototype_tests'));
-//		describe('/static', function () {
-//			container.register('integration_collection_static_filter_test', require('./static/filter.test'));
-//			container.register('integration_collection_static_filter_relations_test', require('./static/filter.relations.test'));
-//			container.register('integration_collection_static_getAll_test', require('./static/getAll.test'));
-//			container.register('integration_collection_static_getAll_relations_test', require('./static/getAll.relations.test'));
-//
-//			describe('filter', function () {
-//				describe('filter.test', container.get('integration_collection_static_filter_test'));
-//				describe('filter.relations.test', container.get('integration_collection_static_filter_relations_test'));
-//			});
-//
-//			describe('getAll', function () {
-//				describe('getAll.test', container.get('integration_collection_static_getAll_test'));
-//				describe('getAll.relations.test', container.get('integration_collection_static_getAll_relations_test'));
-//			});
-//		});
+		describe('/static', function () {
+			container.register('integration_collection_static_filter_test', require('./static/filter.test'));
+			container.register('integration_collection_static_filter_relations_test', require('./static/filter.relations.test'));
+			container.register('integration_collection_static_getAll_test', require('./static/getAll.test'));
+			container.register('integration_collection_static_getAll_relations_test', require('./static/getAll.relations.test'));
+
+			describe('filter', function () {
+				describe('filter.test', container.get('integration_collection_static_filter_test'));
+				describe('filter.relations.test', container.get('integration_collection_static_filter_relations_test'));
+			});
+
+			describe('getAll', function () {
+				describe('getAll.test', container.get('integration_collection_static_getAll_test'));
+				describe('getAll.relations.test', container.get('integration_collection_static_getAll_relations_test'));
+			});
+		});
 	};
 };
